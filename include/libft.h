d47cf14cd1fac453f3be7391808e30f6991f3e9a
CACA <cauansilvaveloso59@gmail.com>
2026-06-03 21:14:24 -0300

    Started my Library

---
+++ include/libft.h
@@ -0,0 +1,18 @@
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# ifdef _cplusplus
+extern "C" {
+# endif
+
+# include "char.h"
+# include "extra.h"
+# include "list.h"
+# include "memory.h"
+# include "string.h"
+
+# ifdef _cplusplus
+}
+# endif
+
+#endif
