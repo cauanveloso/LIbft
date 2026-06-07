9aaf2f8eedf6d6110a2b1bb5bc8352a8e7010218
CACA <cauansilvaveloso59@gmail.com>
2026-06-07 10:09:30 -0300

    finshed partial directory from string on my libft and modify gitignore

--- src/string/ft_strrchr.c
+++ src/string/ft_strrchr.c
@@ -1,1 +1,21 @@
 #include "libft.h"
+
+char	*ft_strrchr(const char *s, int c)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	if (c == '\0' && s[i] == '\0')
+	{
+		return ((char *)&s[i]);
+	}
+	while (i > 0)
+	{
+		if (s[i] == c)
+			return ((char *)&s[i]);
+		i--;
+	}
+	return (NULL);
+}
--- src/string/ft_strtrim.c
+++ src/string/ft_strtrim.c
@@ -1,1 +1,29 @@
 #include "libft.h"
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	size_t	start;
+	size_t	end;
+	size_t	i;
+	char	*dst;
+
+	if (!s1 || !set)
+		return (NULL);
+	start = 0;
+	while (s1[start] && ft_strchr(set, s1[start]))
+		start++;
+	end = ft_strlen(s1);
+	while (end > start && ft_strchr(set, s1[end - 1]))
+		end--;
+	dst = malloc(sizeof(char) * (end - start + 1));
+	if (!dst)
+		return (NULL);
+	i = 0;
+	while (start + i < end)
+	{
+		dst[i] = s1[start + i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (dst);
+}
