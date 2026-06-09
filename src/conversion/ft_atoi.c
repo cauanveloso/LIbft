b1102efa50d586754ad35081cd1e11747df3e662
CACA <cauansilvaveloso59@gmail.com>
2026-06-08 21:36:30 -0300

    Finshed atoi in my-libft

--- src/conversion/ft_atoi.c
+++ src/conversion/ft_atoi.c
@@ -1,3 +1,28 @@
 #include "libft.h"
 
-int		ft_atoi(const char *nptr);
+int		ft_atoi(const char *nptr)
+{
+	int	i;
+	int	sgin;
+	int	result;
+
+	i = 0;
+	sgin = 1;
+	result = 0;
+	while (nptr[i] == 32 || (nptr[i] >= 9 && nptr[i] <= 13))
+		i++;
+	if (nptr[i] == '-' || nptr[i] == '+')
+	{
+		if (nptr[i] == '-')
+		{
+			sgin = sgin * (-1);
+		}
+		i++;
+	}
+	while (nptr[i] >= '0' && nptr[i] <= '9')
+	{
+		result = result * 10 + (nptr[i] - '0');
+		i++;
+	}
+	return (result * sgin);
+}
