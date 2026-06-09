ab4c3093a6d98cd729ecddd9deb954767a32db2f
Cauan-42PC <cauan.s.dev@gmail.com>
2026-06-09 13:51:50 -0300

    Finshed all functions in conversion directory

--- src/conversion/ft_itoa.c
+++ src/conversion/ft_itoa.c
@@ -1,3 +1,49 @@
 #include "libft.h"
 
-char	*ft_itoa(int n);
+size_t	ft_numlen(long n)
+{
+	size_t	len;
+
+	if (n == 0)
+		return (1);
+	len = 0;
+	if (n < 0)
+	{
+		len++;
+		n *= -1;
+	}
+	while (n > 0)
+	{
+		n /= 10;
+		len++;
+	}
+	return (len);
+}
+
+char	*ft_itoa(int n)
+{
+	long	nb;
+	char	*dst;
+	size_t	size;
+	size_t	neg;
+
+	nb = n;
+	size = ft_numlen(nb);
+	dst = malloc(sizeof(char) * (size + 1));
+	if (!dst)
+		return (NULL);
+	dst[size] = '\0';
+	neg = (nb < 0);
+	if (neg)
+	{
+		nb *= -1;
+		dst[0] = '-';
+	}
+	while (size > neg)
+	{
+		size--;
+		dst[size] = nb % 10 + '0';
+		nb /= 10;
+	}
+	return (dst);
+}
