4fb2a8220f2a7394eb55e3e997d50708f732129a
CACA <cauansilvaveloso59@gmail.com>
2026-07-05 18:52:53 -0300

    <fix>[src/string/ft_strjoin.c]<Added a check in ft_strjoin for cases where string s1 or string s2 fails.s>

--- src/string/ft_strjoin.c
+++ src/string/ft_strjoin.c
@@ -6,6 +6,8 @@ char *ft_strjoin(char const *s1, char const *s2)
 	size_t	j;
 	char	*dst;
 
+	if (!s1 || !s2)
+		return (NULL);
 	dst = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
 	if (!dst)
 		return (NULL);
